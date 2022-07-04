e6823c11c80e3a624cf4f2e718799341f58f33fc
gugsense508 <108687859+gugsense508@users.noreply.github.com>
2022-07-05 00:59:56 +0700

    Create gogok.sh

---
+++ gogok.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+echo "Start Deploy" && sleep 30
+echo "2 Deploy" && sleep 30
+echo "3 Deploy" && sleep 30
+echo "4 Deploy In Progress..." && sleep 30
+echo "5 Deploy In Progress..." && sleep 30
+echo "6 Deploy In Progress..." && sleep 30
+echo "7 Deploy In Progress..." && sleep 30
+echo "8 Deploy In Progress..." && sleep 30
+echo "9 Deploy In Progress..." && sleep 30
+echo "10 Menit deploy..." && sleep 30
+echo "11 Menit deploy..." && sleep 30
+echo "12 Deploy" && sleep 30
+echo "13 Deploy" && sleep 30
+echo "14 Deploy In Progress..." && sleep 30
+echo "15 Deploy In Progress..." && sleep 30
+echo "16 Deploy In Progress..." && sleep 30
+echo "17 Deploy In Progress..." && sleep 30
+echo "18 Deploy In Progress..." && sleep 30
+echo "19 Deploy In Progress..." && sleep 30
+echo "20 Menit deploy..." && sleep 30
+echo "21 Menit deploy..." && sleep 30
+echo "22 Deploy" && sleep 30
+echo "23 Deploy" && sleep 30
+echo "24 Deploy In Progress..." && sleep 30
+echo "25 Deploy In Progress..." && sleep 30
+echo "26 Deploy In Progress..." && sleep 30
+echo "27 Deploy In Progress..." && sleep 30
+echo "28 Deploy In Progress..." && sleep 30
+echo "29 Deploy In Progress..." && sleep 30
+echo "30 Menit deploy..." && sleep 30
+echo "31 Menit deploy..." && sleep 30
+echo "32 Deploy" && sleep 30
+echo "33 Deploy" && sleep 30
+echo "34 Deploy In Progress..." && sleep 30
+echo "35 Deploy In Progress..." && sleep 30
+echo "36 Deploy In Progress..." && sleep 30
+echo "37 Deploy In Progress..." && sleep 30
+echo "38 Deploy In Progress..." && sleep 30
+echo "39 Deploy In Progress..." && sleep 30
+echo "40 Menit deploy..." && sleep 30
+echo "41 Menit deploy..." && sleep 30
+echo "42 Deploy" && sleep 30
+echo "43 Deploy" && sleep 30
+echo "44 Deploy In Progress..." && sleep 30
+echo "45 Deploy In Progress..." && sleep 30
+echo "46 Deploy In Progress..." && sleep 30
+echo "47 Deploy In Progress..." && sleep 30
+echo "48 Deploy In Progress..." && sleep 30
+echo "49 Deploy In Progress..." && sleep 30
+echo "50 Menit deploy..." && sleep 30
+echo "51 Menit deploy..." && sleep 30
+echo "52 Deploy" && sleep 30
+echo "53 Deploy" && sleep 30
+echo "54 Deploy In Progress..." && sleep 30
+echo "55 Deploy In Progress..." && sleep 30
+echo "56 Deploy In Progress..." && sleep 30
+echo "57 Deploy In Progress..." && sleep 30
+echo "58 Deploy In Progress..." && sleep 30
+echo "59 Deploy In Progress..." && sleep 30
+echo "Restart Deploy" && sleep 30
+echo "2 Deploy" && sleep 30
+echo "3 Deploy" && sleep 30
+echo "4 Deploy In Progress..." && sleep 30
+echo "5 Deploy In Progress..." && sleep 30
+echo "6 Deploy In Progress..." && sleep 30
+echo "7 Deploy In Progress..." && sleep 30
+echo "8 Deploy In Progress..." && sleep 30
+echo "9 Deploy In Progress..." && sleep 30
+echo "10 Menit deploy..." && sleep 30
+echo "11 Menit deploy..." && sleep 30
+echo "12 Deploy" && sleep 30
+echo "13 Deploy" && sleep 30
+echo "14 Deploy In Progress..." && sleep 30
+echo "15 Deploy In Progress..." && sleep 30
+echo "16 Deploy In Progress..." && sleep 30
+echo "17 Deploy In Progress..." && sleep 30
+echo "18 Deploy In Progress..." && sleep 30
+echo "19 Deploy In Progress..." && sleep 30
+echo "20 Menit deploy..." && sleep 30
+echo "21 Menit deploy..." && sleep 30
+echo "22 Deploy" && sleep 30
+echo "23 Deploy" && sleep 30
+echo "24 Deploy In Progress..." && sleep 30
+echo "25 Deploy In Progress..." && sleep 30
+echo "26 Deploy In Progress..." && sleep 30
+echo "27 Deploy In Progress..." && sleep 30
+echo "28 Deploy In Progress..." && sleep 30
+echo "29 Deploy In Progress..." && sleep 30
+echo "30 Menit deploy..." && sleep 30
+echo "31 Menit deploy..." && sleep 30
+echo "32 Deploy" && sleep 30
+echo "33 Deploy" && sleep 30
+echo "34 Deploy In Progress..." && sleep 30
+echo "35 Deploy In Progress..." && sleep 30
+echo "36 Deploy In Progress..." && sleep 30
+echo "37 Deploy In Progress..." && sleep 30
+echo "38 Deploy In Progress..." && sleep 30
+echo "39 Deploy In Progress..." && sleep 30
+echo "40 Menit deploy..." && sleep 30
+echo "41 Menit deploy..." && sleep 30
+echo "42 Deploy" && sleep 30
+echo "43 Deploy" && sleep 30
+echo "44 Deploy In Progress..." && sleep 30
+echo "45 Deploy In Progress..." && sleep 30
+echo "46 Deploy In Progress..." && sleep 30
+echo "47 Deploy In Progress..." && sleep 30
+echo "48 Deploy In Progress..." && sleep 30
+echo "49 Deploy In Progress..." && sleep 30
+echo "50 Menit deploy..." && sleep 30
+echo "51 Menit deploy..." && sleep 30
+echo "52 Deploy" && sleep 30
+echo "53 Deploy" && sleep 30
+echo "54 Deploy In Progress..." && sleep 30
+echo "55 Deploy In Progress..." && sleep 30
+echo "56 Deploy In Progress..." && sleep 30
+echo "57 Deploy In Progress..." && sleep 30
+echo "58 Deploy In Progress..." && sleep 30
+echo "59 Deploy In Progress..." && sleep 30
+sync && echo 3 | sudo tee /proc/sys/vm/drop_caches
+sudo apt-get autoclean
+sudo apt-get autoremove
+swapoff -a && swapon -a
+echo "Deploy Finish"
